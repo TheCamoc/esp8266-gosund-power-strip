3041c61b83104e7b6be5cd22819101fd8265414d
Chloe <chloe.pr@pm.me>
2022-12-09 20:59:34 +0100

    remove restart path from server

--- src/main.cpp
+++ src/main.cpp
@@ -104,12 +104,6 @@ void setup()
     ESPTools.addConfigString("mqtt_topic_relay_3");
     ESPTools.addConfigString("mqtt_topic_relay_4");
 
-    server.on("/restart", [&]()
-              {
-        server.send(200, "text/plain", "Ok");
-        delay(500);
-        ESP.restart(); });
-
     server.begin();
 
     // Setup MQTT
